a835fb6aaeb7c4144c8bb4dc41b330b1052a7204
Jack Leow <jackgene@gmail.com>
2020-03-16 19:40:32 -0700

    Updated arm-elf-binutils.rb to use the actual sha256 of the file (key was updated to "sha256", but the value was still sha1).

--- arm-elf-binutils.rb
+++ arm-elf-binutils.rb
@@ -3,7 +3,7 @@ require 'formula'
 class ArmElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz'
-  sha256 'f10c64e92d9c72ee428df3feaf349c4ecb2493bd'
+  sha256 'f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952'
 
   depends_on 'gcc' => :build
   def install
